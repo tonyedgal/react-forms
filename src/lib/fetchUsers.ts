3053dfe2c5077c16aa4eb4e56b96ecda38c5c5ef
tonyedgal <tonyedgal@gmail.com>
2023-05-28 22:52:45 +0100

    feat: infer types, fetch and parse data with zod

---
+++ src/lib/fetchUsers.ts
@@ -0,0 +1,24 @@
+import { BasicUserSchema } from "../models/User";
+import { z } from "zod";
+
+const UserResults = z.array(BasicUserSchema);
+
+type UserArray = z.infer<typeof UserResults>;
+
+export default async function fetchUsers(): Promise<UserArray | undefined> {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!res.ok) return undefined;
+
+    const usersJson: UserArray = await res.json();
+
+    const parsedData = UserResults.parse(usersJson);
+
+    console.log(parsedData);
+
+    return parsedData;
+  } catch (error) {
+    if (error instanceof Error) console.log(error.stack);
+  }
+}
