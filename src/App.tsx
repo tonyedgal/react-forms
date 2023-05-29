800c09a51da52f5598f3e534be09a4a9cf54bcfb
tonyedgal <tonyedgal@gmail.com>
2023-05-30 00:51:20 +0100

    feat: form validation with zodResolver and RHF

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Form from "./components/Form";
 import fetchUsers from "./lib/fetchUsers";
 
 fetchUsers();
 
 function App() {
-  return <main className="bg-slate-700 min-h-screen p-5"></main>;
+  return (
+    <main className="bg-slate-700 min-h-screen p-5">
+      <Form />
+    </main>
+  );
 }
 
 export default App;
