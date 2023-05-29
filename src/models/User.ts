97ce90bc00bac5a8f737982a833ade3cf11c30c1
tonyedgal <tonyedgal@gmail.com>
2023-05-29 05:22:18 +0100

    feat: extending and merging types with zod

--- src/models/User.ts
+++ src/models/User.ts
@@ -39,3 +39,39 @@ export const BasicUserSchema = z.object({
     catchPhrase: z.string().optional(),
   }),
 });
+
+const UserAddressSchema = z.object({
+  street: z.string().trim().min(5, {
+    message: "Street name must be 5 or more characters long",
+  }),
+  suite: z.string().trim().optional(),
+  city: z.string().trim().min(2, {
+    message: "City name must be 2 or more characters long",
+  }),
+  zipcode: z.string().regex(/^\d{5}(?:[-\s]\d{4})?$/, {
+    message: "Must be 5 digit zip. Optional 4 digit extension allowed",
+  }),
+});
+
+const UserAddressSchemaWithGeo = UserAddressSchema.extend({
+  geo: z.object({
+    lat: z.string(),
+    lng: z.string(),
+  }),
+});
+
+const HasIDSchema = z.object({
+  id: z.number().int().positive(),
+});
+
+export const UserSchemaWithAddress = BasicUserSchema.extend({
+  address: UserAddressSchema,
+}).merge(HasIDSchema);
+
+export const UserSchemaWithGeo = BasicUserSchema.extend({
+  address: UserAddressSchemaWithGeo,
+}).merge(HasIDSchema);
+
+export type UserWithAddress = z.infer<typeof UserSchemaWithAddress>;
+
+export type UserWithGeo = z.infer<typeof UserSchemaWithGeo>;
