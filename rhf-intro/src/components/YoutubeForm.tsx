07eed3ede838610308b33491d91970572bbba1ec
tonyedgal <tonyedgal@gmail.com>
2023-06-06 13:24:59 +0100

    feat: add devtools to form component

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -1,8 +1,9 @@
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const YoutubeForm = () => {
   const form = useForm();
-  const { register } = form;
+  const { register, control } = form;
 
   return (
     <div>
@@ -18,6 +19,7 @@ const YoutubeForm = () => {
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
