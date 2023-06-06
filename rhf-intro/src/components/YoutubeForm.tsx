a9630efe54009f2200efd8f03c23231fdf6b0417
tonyedgal <tonyedgal@gmail.com>
2023-06-06 12:27:41 +0100

    feat: handle form state with `register` function

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
 const YoutubeForm = () => {
+  const form = useForm();
+  const { register } = form;
+
   return (
     <div>
       <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
