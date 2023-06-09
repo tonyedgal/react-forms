43a18a55dbbded726cbb503250699db8c2da87e4
tonyedgal <tonyedgal@gmail.com>
2023-06-09 15:21:56 +0100

    feat: tracking form state and rerenders

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0;
 
 const YoutubeForm = () => {
   const form = useForm();
   const { register, control } = form;
 
+  const [render, setRender] = useState("");
+
+  renderCount++;
+
   return (
     <div>
+      <h1>Form ({renderCount / 2})</h1>
       <form>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
@@ -16,6 +24,15 @@ const YoutubeForm = () => {
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
+
+        <label htmlFor="render">Render</label>
+        <input
+          type="text"
+          id="render"
+          name="render"
+          value={render}
+          onChange={(e) => setRender(e.target.value)}
+        />
 
         <button>Submit</button>
       </form>
