1714012c53da91dc94a90822cc422b72406d83e4
tonyedgal <tonyedgal@gmail.com>
2023-06-11 20:05:39 +0100

    feat: displaying error messages with RHF

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -9,7 +9,8 @@ type FormValues = {
 
 const YoutubeForm = () => {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log("form submitted: ", data);
@@ -18,31 +19,51 @@ const YoutubeForm = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            required: "Username is required",
-            // this can also be added as an object, just like the pattern object below
-          })}
-        />
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "Username is required",
+              },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: {
+                value: true,
+                message: "Channel is required",
+              },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
 
         <button>Submit</button>
       </form>
