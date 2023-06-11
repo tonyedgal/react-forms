a9efb0c6cfcfbccc3c7b5ad7f2f1bca71a37cd80
tonyedgal <tonyedgal@gmail.com>
2023-06-11 13:50:54 +0100

    feat: addinbg form validation in RHF

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -17,12 +17,29 @@ const YoutubeForm = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: "Username is required",
+            // this can also be added as an object, just like the pattern object below
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
