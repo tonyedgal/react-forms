8d4e6a1e35331d57a2b46a1b72ea13fa6a69eb36
tonyedgal <tonyedgal@gmail.com>
2023-06-16 22:48:58 +0100

    feat: working with arrays in RHF

--- rhf-intro/src/components/YoutubeForm.tsx
+++ rhf-intro/src/components/YoutubeForm.tsx
@@ -9,6 +9,7 @@ type FormValues = {
     twitter: string;
     facebook: string;
   };
+  phoneNumbers: Array<string>;
 };
 
 const YoutubeForm = () => {
@@ -21,6 +22,7 @@ const YoutubeForm = () => {
         twitter: "",
         facebook: "",
       },
+      phoneNumbers: ["", ""],
     },
   });
   const { register, control, handleSubmit, formState } = form;
@@ -101,6 +103,24 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="facebook">Facebook</label>
           <input type="text" id="facebook" {...register("social.facebook")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primary-phone">Primary phone number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0")}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondary-phone">Secondary phone number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1")}
+          />
         </div>
 
         <button>Submit</button>
